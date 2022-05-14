696d8de78865cfd840a6065358c5bbda47c53d1f
Kamil Kuziora <kam.kuziora@gmail.com>
2022-05-15 00:44:07 +0200

    server won't crash on bad request

--- task8/src/server.c
+++ task8/src/server.c
@@ -20,12 +20,46 @@ void sig_handler() {
     exit_handler();
     _exit(1);
 }
+int str_to_num (char* str, int* num) {
+    char* error = NULL;
+    *num = strtod(str, &error);
+    if (*error != '\0') {
+        return -1;
+    }
+    return 0;
+}
 /*funkcja odpowiedzialna za interpretowanie otrzymanej od kilenta wiadomości*/
-void input_parser(char *str,char* queue_name, int* num1, int* num2, char* operator) {
-    strcpy(queue_name, strtok(str, " "));
-    *num1 = atoi(strtok(NULL, " "));
-    *operator = strtok(NULL, " ")[0];
-    *num2 = atoi(strtok(NULL, " "));
+int input_parser(char *str,char* queue_name, int* num1, int* num2, char* operator) {
+    char *tmp = strtok(str, " ");
+    int converted_num;
+    if (tmp == NULL) {
+        return -1;
+    }
+    strcpy(queue_name, tmp);
+    tmp = strtok(NULL, " ");
+    if (tmp == NULL) {
+        return -1;
+    }
+    if (str_to_num(tmp, &converted_num) == -1) {
+        return -1;
+    }
+    *num1 = converted_num;
+    tmp = strtok(NULL, " ");
+    if (tmp == NULL) {
+        return -1;
+    }
+    *operator = tmp[0];
+    tmp = strtok(NULL, " ");
+    if (tmp == NULL) {
+        return -1;
+    }
+    tmp[strlen(tmp) - 1] = '\0';
+    if (str_to_num(tmp, &converted_num) == -1) {
+        return -1;
+    }
+
+    *num2 = converted_num;
+    return 0;
 }
 /*funkcja zwracająca wynik zapytania klienta*/
 int compute (int num1, char operator, int num2) {
@@ -49,6 +83,7 @@ int main() {
     char client_queue_name[20];
     int num1;
     int num2;
+    int parser_return_value;
     char operator;
     mqd_t client_queue;
 
@@ -70,23 +105,26 @@ int main() {
         /*odbieranie zapytania od klienta*/ 
         msg_receive(queue, buffer);
         /*przetwarzanie otrzymanych danych*/
-        input_parser(buffer, client_queue_name, &num1, &num2, &operator);
-
+        parser_return_value = input_parser(buffer, client_queue_name, &num1, &num2, &operator);
         printf("[SERVER] >> otrzymano zapytanie od procesu: %s >> TREŚĆ: %d %c %d\n", client_queue_name, num1, operator, num2);
-       
+           
         /*otwarcie kolejki klienta*/
         client_queue = msg_open(client_queue_name, O_WRONLY);
-
-        /*usunięcie znaku '/' z nazwy*/
-        memmove(client_queue_name, client_queue_name + 1, strlen(client_queue_name));
-
         printf("[SERVER] >> otworzono kolejkę klienta o PID: %s\n", client_queue_name);
 
-        sprintf(buffer, "%d", compute(num1, operator, num2));
-        
+        /*walidacja poprawności składni polecenia*/
+        if (parser_return_value == 0) {
+            /*usunięcie znaku '/' z nazwy*/
+            memmove(client_queue_name, client_queue_name + 1, strlen(client_queue_name));
+            
+            /*wykonanie obliczenia i zapisanie do bufora*/
+            sprintf(buffer, "%d", compute(num1, operator, num2));
+        } else {
+            sprintf(buffer, "[ERROR] >> niepoprawna składnia zapytania\n");
+        }
+
         /*wysłanie odpowiedzie na zapytanie klienta*/
         msg_send(client_queue, buffer);
-
         printf("[SERVER] >> wysłano odpowiedź do klienta o PID: %s\n", client_queue_name);
 
         /*zamknięcie kolejki klienta*/ 
